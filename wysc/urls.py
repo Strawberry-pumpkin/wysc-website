2867f89591c211a1104e1a729711fce979893c0f
raditha dissanayake <raditha@raditha.com>
2024-05-24 09:17:25 +0530

    Now the html is delivered as rendered django templates

--- wysc/urls.py
+++ wysc/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # render the template in wysc/templaces/index.html when the root URL is accessed
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('wysc', TemplateView.as_view(template_name='wysc.html'), name='home'),
 ]
